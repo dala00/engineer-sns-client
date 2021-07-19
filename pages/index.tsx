20078207af2580cb598ae82d9aa71654272b1097
Shoichi Namba <alphabrend@gmail.com>
2021-07-19 22:54:33 +0900

    ワーニング修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
         loadMore={loadMore}
         hasMore={hasMore}
         loader={
-          <Center>
+          <Center key="spinner">
             <Spinner />
           </Center>
         }
