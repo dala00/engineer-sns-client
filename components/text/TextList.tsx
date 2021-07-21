894ea2b74763f92ddfbd9d3a1bb048851a1cd5f2
Shoichi Namba <alphabrend@gmail.com>
2021-07-21 21:32:04 +0900

    URL対応

--- components/text/TextList.tsx
+++ components/text/TextList.tsx
@@ -4,6 +4,7 @@ import { useCallback } from 'react'
 import { Text } from '../../models/text'
 import { User } from '../../models/user'
 import Card from '../Card'
+import TextListRow from './TextListRow'
 
 type Props = {
   texts: Text[]
@@ -11,39 +12,15 @@ type Props = {
 }
 
 export default function TextList(props: Props) {
-  const getUserName = useCallback(
-    (userId: string) => {
-      const user = props.users.find((user) => user.id === userId)
-      if (user) {
-        return user.name
-      }
-      return undefined
-    },
+  const getUser = useCallback(
+    (userId: string) => props.users.find((user) => user.id === userId),
     [props.users]
   )
 
   return (
     <Box>
       {props.texts.map((text) => (
-        <Card key={text.id} my={4} whiteSpace="pre-wrap">
-          <>
-            <Flex alignItems="center" mb={2}>
-              {getUserName(text.userId) ? (
-                <Box fontSize={10} color="gray.700" mr={2}>
-                  {getUserName(text.userId)}
-                </Box>
-              ) : (
-                <Box fontSize={9} color="gray.700" mr={2}>
-                  {text.userId}
-                </Box>
-              )}
-              <Box fontSize={9} color="gray.700" display="inline-block">
-                {text.createdAt.format('YYYY/MM/DD HH:mm:ss')}
-              </Box>
-            </Flex>
-            <Box>{text.text}</Box>
-          </>
-        </Card>
+        <TextListRow key={text.id} text={text} user={getUser(text.userId)} />
       ))}
     </Box>
   )
---
+++ components/text/TextListRow.tsx
@@ -0,0 +1,87 @@
+import { Box, Flex, Image, Link } from '@chakra-ui/react'
+import React from 'react'
+import { useCallback } from 'react'
+import { Text } from '../../models/text'
+import { User } from '../../models/user'
+import Card from '../Card'
+
+type Props = {
+  text: Text
+  user?: User
+}
+
+type ParsedText = {
+  body: string
+  urls: string[]
+  imageUrls: string[]
+}
+
+export default function TextListRow(props: Props) {
+  const { text, user } = props
+
+  const parseText = useCallback((): ParsedText => {
+    const matches = text.text.match(
+      /https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g
+    )
+    if (!matches) {
+      return {
+        body: text.text,
+        urls: [],
+        imageUrls: [],
+      }
+    }
+
+    let body = text.text
+    const urls = []
+    const imageUrls = []
+    matches.forEach((url) => {
+      body = body.replace(url, '')
+      if (url.match(/\.(png|jpe?g|gif|svg)$/)) {
+        imageUrls.push(url)
+      } else {
+        urls.push(url)
+      }
+    })
+    return { body: body.trim(), urls, imageUrls }
+  }, [text])
+
+  const parsedText = parseText()
+
+  return (
+    <Card key={text.id} my={4} whiteSpace="pre-wrap">
+      <>
+        <Flex alignItems="center" mb={2}>
+          {user ? (
+            <Box fontSize={10} color="gray.700" mr={2}>
+              {user.name}
+            </Box>
+          ) : (
+            <Box fontSize={9} color="gray.700" mr={2}>
+              {text.userId}
+            </Box>
+          )}
+          <Box fontSize={9} color="gray.700" display="inline-block">
+            {text.createdAt.format('YYYY/MM/DD HH:mm:ss')}
+          </Box>
+        </Flex>
+        <Box>{parsedText.body}</Box>
+        {parsedText.urls.length > 0 && (
+          <Box mt={2}>
+            {parsedText.urls.map((url) => (
+              <Box key={url}>
+                <Link href={url} color="teal.500" isExternal>
+                  {url}
+                </Link>
+              </Box>
+            ))}
+          </Box>
+        )}
+        {parsedText.imageUrls.map((url) => (
+          <Box key={url} my={2}>
+            <Image src={url} alt="" />
+          </Box>
+        ))}
+      </>
+    </Card>
+  )
+}
